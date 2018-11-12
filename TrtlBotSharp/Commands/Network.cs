003f24a3a156f1ae4c747910ef3ea39454ede2ff
MaddestHatter <44808557+MaddestHatter@users.noreply.github.com>
2018-11-12 13:11:23 +0000

    Clean up Network Commands

--- TrtlBotSharp/Commands/Network.cs
+++ TrtlBotSharp/Commands/Network.cs
@@ -9,12 +9,7 @@ namespace TrtlBotSharp
         [Command("hashrate")]
         public async Task HashrateAsync([Remainder]string Remainder = "")
         {
-            // Get last block header from daemon and calculate hashrate
-            decimal Hashrate = 0;
-            JObject Result = Request.RPC(TrtlBotSharp.daemonHost, TrtlBotSharp.daemonPort, "getlastblockheader");
-            if (Result.Count > 0 && !Result.ContainsKey("error"))
-                Hashrate = (decimal)Result["block_header"]["difficulty"] / 120;
-
+	    decimal Hashrate = TrtlBotSharp.GetHashrate();	
             // Send reply
             await ReplyAsync("The current global hashrate is **" + TrtlBotSharp.FormatHashrate(Hashrate) + "**");
         }
@@ -22,12 +17,7 @@ namespace TrtlBotSharp
         [Command("difficulty")]
         public async Task DifficultyAsync([Remainder]string Remainder = "")
         {
-            // Get last block header from daemon and calculate hashrate
-            decimal Difficulty = 0;
-            JObject Result = Request.RPC(TrtlBotSharp.daemonHost, TrtlBotSharp.daemonPort, "getlastblockheader");
-            if (Result.Count > 0 && !Result.ContainsKey("error"))
-                Difficulty = (decimal)Result["block_header"]["difficulty"];
-
+	    decimal Difficulty = TrtlBotSharp.GetDifficulty();
             // Send reply
             await ReplyAsync(string.Format("The current difficulty is **{0:N0}**", Difficulty));
         }
@@ -35,12 +25,7 @@ namespace TrtlBotSharp
         [Command("height")]
         public async Task HeightAsync([Remainder]string Remainder = "")
         {
-            // Get height
-            decimal Height = 0;
-            JObject Result = Request.GET("http://" + TrtlBotSharp.daemonHost + ":" + TrtlBotSharp.daemonPort + "/getinfo");
-            if (Result.Count > 0 && !Result.ContainsKey("error"))
-                Height = (decimal)Result["height"];
-
+           decimal Height = TrtlBotSharp.GetHeight();
 
             // Send reply
             await ReplyAsync(string.Format("The current block height is **{0:N0}**", Height));
@@ -69,18 +54,9 @@ namespace TrtlBotSharp
 		   	     "\nThe current global hashrate is **" + TrtlBotSharp.FormatHashrate(Hashrate) + "**" + 
 			     "\nThe current difficulty is **{1:N0}**" + 
 			     "\nThe current circulating supply is **{2:N4}** {3}", Height, Difficulty, Supply, TrtlBotSharp.coinSymbol ); 
-
-	    // Send reply
-	    //await ReplyAsync(string.Format("The current block height is **{0:N0}**", Height));
-            //await ReplyAsync("The current global hashrate is **" + TrtlBotSharp.FormatHashrate(Hashrate) + "**"); 
-	    //await ReplyAsync(string.Format("The current difficulty is **{0:N0}**", Difficulty)); 
-	    //await ReplyAsync(string.Format("The current circulating supply is **{0:N4}** {1}", Supply, TrtlBotSharp.coinSymbol));
 	    
 	    await ReplyAsync(string.Format(Message));
 
 	}
-    
-
     }
-
 }
